b4f728dafa2a8be490bd82159b3ab63a61d99da2
HeheVeds <vedikapatil7706@gmail.com>
2026-08-19 15:49:45 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,203 @@
+#for 'b' typecode
+from array import array
+
+a = array('b', [10, 20, 30, 20])
+
+print("Original:", a)
+
+a.append(40)
+print("Append:", a)
+
+a.insert(1, 15)
+print("Insert:", a)
+
+a.remove(20)
+print("Remove:", a)
+
+a.pop()
+print("Pop:", a)
+
+print("Count of 20:", a.count(20))
+
+a.extend([50, 60])
+print("Extend:", a)
+
+print("Index of 30:", a.index(30))
+
+a.reverse()
+print("Reverse:", a)
+
+
+#for 'h' typecode
+from array import array
+
+a = array('h', [10, 20, 30, 20])
+
+print("Original:", a)
+
+a.append(40)
+print("Append:", a)
+
+a.insert(1, 15)
+print("Insert:", a)
+
+a.remove(20)
+print("Remove:", a)
+
+a.pop()
+print("Pop:", a)
+
+print("Count of 20:", a.count(20))
+
+a.extend([50, 60])
+print("Extend:", a)
+
+print("Index of 30:", a.index(30))
+
+a.reverse()
+print("Reverse:", a)
+
+#for 'i' typecode
+from array import array
+
+a = array('i', [10, 20, 30, 20])
+
+print("Original:", a)
+
+a.append(40)
+print("Append:", a)
+
+a.insert(1, 15)
+print("Insert:", a)
+
+a.remove(20)
+print("Remove:", a)
+
+a.pop()
+print("Pop:", a)
+
+print("Count of 20:", a.count(20))
+
+a.extend([50, 60])
+print("Extend:", a)
+
+print("Index of 30:", a.index(30))
+
+a.reverse()
+print("Reverse:", a)
+
+#for 'l' typecode
+from array import array
+
+a = array('l', [10, 20, 30, 20])
+
+print("Original:", a)
+
+a.append(40)
+print("Append:", a)
+
+a.insert(1, 15)
+print("Insert:", a)
+
+a.remove(20)
+print("Remove:", a)
+
+a.pop()
+print("Pop:", a)
+
+print("Count of 20:", a.count(20))
+
+a.extend([50, 60])
+print("Extend:", a)
+
+print("Index of 30:", a.index(30))
+
+a.reverse()
+print("Reverse:", a)
+
+#for 'q' typecode
+from array import array
+
+a = array('q', [10, 20, 30, 20])
+
+print("Original:", a)
+
+a.append(40)
+print("Append:", a)
+
+a.insert(1, 15)
+print("Insert:", a)
+
+a.remove(20)
+print("Remove:", a)
+
+a.pop()
+print("Pop:", a)
+
+print("Count of 20:", a.count(20))
+
+a.extend([50, 60])
+print("Extend:", a)
+
+print("Index of 30:", a.index(30))
+
+a.reverse()
+print("Reverse:", a)
+
+#for 'f' typecode
+from array import array
+
+a = array('f', [10.5, 20.5, 30.5, 20.5])
+
+print("Original:", a)
+
+a.append(40.5)
+print("Append:", a)
+
+a.insert(1, 15.5)
+print("Insert:", a)
+
+a.remove(20.5)
+print("Remove:", a)
+
+a.pop()
+print("Pop:", a)
+
+print("Count of 20.5:", a.count(20.5))
+
+a.extend([50.5, 60.5])
+print("Extend:", a)
+
+print("Index of 30.5:", a.index(30.5))
+
+a.reverse()
+print("Reverse:", a)
+
+#for 'd' typecode
+from array import array
+
+a = array('d', [10.5, 20.5, 30.5, 20.5])
+
+print("Original:", a)
+
+a.append(40.5)
+print("Append:", a)
+
+a.insert(1, 15.5)
+print("Insert:", a)
+
+a.remove(20.5)
+print("Remove:", a)
+
+a.pop()
+print("Pop:", a)
+
+print("Count of 20.5:", a.count(20.5))
+
+a.extend([50.5, 60.5])
+print("Extend:", a)
+
+print("Index of 30.5:", a.index(30.5))
+
+a.reverse()
+print("Reverse:", a)
